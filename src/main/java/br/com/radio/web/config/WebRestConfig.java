c392281f62b43b083e28ad510986a6777d479339
Fernando Pazin <pazinfernando@gmail.com>
2015-10-07 11:59:56 -0300

    experimentando com spring-data-rest

---
+++ src/main/java/br/com/radio/web/config/WebRestConfig.java
@@ -0,0 +1,8 @@
+package br.com.radio.web.config;
+
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+// para exportar serviços de REST basta adicionar o @configuration
+public class WebRestConfig extends RepositoryRestMvcConfiguration {
+
+}
