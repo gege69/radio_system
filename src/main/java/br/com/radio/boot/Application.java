684f1431ff66a1689441e193d454a25fd9104a3d
Fernando Pazin <pazinfernando@gmail.com>
2016-02-27 10:30:15 -0300

    alteração pra deploy

--- src/main/java/br/com/radio/boot/Application.java
+++ src/main/java/br/com/radio/boot/Application.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import br.com.radio.model.Ambiente;
 import br.com.radio.model.Usuario;
+import br.com.radio.repository.AmbienteRepository;
 import br.com.radio.repository.UsuarioRepository;
 import br.com.radio.service.AmbienteService;
 import br.com.radio.service.ProgramacaoMusicalService;
@@ -30,13 +31,50 @@ public class Application {
 	public static void main(String[] aaaa)
 	{
 		ApplicationContext ctx = SpringApplication.run(Application.class, aaaa);
-
 		Logger.getLogger("org.jaudiotagger").setLevel(Level.OFF);
 		
 //		criarVariosAmbientes( ctx );
+		
+//		criaProgramacaoVariosAmbientes( ctx );
 																																																								
 	}
 
+	
+
+	private static void criaProgramacaoVariosAmbientes( ApplicationContext ctx )
+	{
+//		UsuarioRepository usuRepo = ctx.getBean( UsuarioRepository.class );
+		AmbienteRepository ambRepo = ctx.getBean( AmbienteRepository.class );
+//		AmbienteService ambienteService = ctx.getBean( AmbienteService.class );
+		ProgramacaoMusicalService progMusicService = ctx.getBean( ProgramacaoMusicalService.class );
+		
+//		Usuario usu = usuRepo.findByLogin( "fpazin" );
+	
+		Integer total = 200;
+
+		Integer inicio = 5;
+
+		for ( int i = inicio; i < total; i++ )
+		{
+
+			try
+			{
+				Ambiente salvo = ambRepo.findOne( new Long(i) );
+				
+				progMusicService.geraTransmissao( salvo );;
+
+				System.out.println(salvo);
+			}
+			catch ( Exception e )
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("fim");
+	}
+	
+	
 	private static void criarVariosAmbientes( ApplicationContext ctx )
 	{
 		UsuarioRepository usuRepo = ctx.getBean( UsuarioRepository.class );
--- src/main/webapp/WEB-INF/views/versao.jsp
+++ src/main/webapp/WEB-INF/views/versao.jsp
@@ -1,5 +1,6 @@
   <div class="row row-centered">
     <div class="col-centered" style="font-size: x-small;">
-      versão 0.2 14/02/2016 20:15:15 PM
+      versão 0.3 27/02/2016 08:55:15 AM
+
     </div>
   </div>
