e3f61a01d4be4fb29abb6c90e7232bd593140486
Fernando Pazin <pazinfernando@gmail.com>
2016-03-12 10:59:45 -0300

    correcao para puxar musicas

--- src/main/java/br/com/radio/service/MidiaService.java
+++ src/main/java/br/com/radio/service/MidiaService.java
@@ -236,8 +236,8 @@ public class MidiaService {
 					troca = true;
 				}
 
-				if ( troca )
-				{
+//				if ( troca )
+//				{
 					midiaGeneros.clear();
 					
 					max = rand.nextInt(2) + 1;
@@ -254,15 +254,15 @@ public class MidiaService {
 						
 						midiaGeneros.add( mg );
 					}
-				}
-				else
-				{
-					for ( MidiaGenero mg : midiaGeneros )
-					{
-						mg.setIdMediagen( null );
-						mg.setMidia( midia );
-					}
-				}
+//				}
+//				else
+//				{
+//					for ( MidiaGenero mg : midiaGeneros )
+//					{
+//						mg.setIdMediagen( null );
+//						mg.setMidia( midia );
+//					}
+//				}
 
 				
 				midiaGeneroRepo.save( midiaGeneros );
